287482167676d17d7f65e1129fcd797d8e59d0c4
Anna Kova <m3ta007@gmail.com>
2021-01-08 16:11:16 -0500

    added 03-loops

---
+++ 03-loops.js
@@ -0,0 +1,51 @@
+/** 
+ * 1.Вывести в консоль сумму всех целых чисел от 50 до 100. */
+
+console.log('=== Sum of integers 50 through 100: ===');
+
+let sum50 = 0
+let i = 50
+
+while (i <= 100) {
+  sum50 += i
+
+  i++
+}
+
+console.log(sum50);
+
+/**
+ * 2.Вывести в консоль таблицу умножения на 7. 7 x1 = 7
+ * 7 x 2 = 14
+ * ...
+ * 7 x 9 = 63 */
+
+console.log('=== Seven times table: ===');
+
+for (let j = 1; j <= 9; j++) {
+  console.log(`7 x ${j} = ${j * 7}`);
+}
+
+/** 
+ * 3* Запросить у пользователя ввод числа N. Вывести в консоль среднее 
+ * арифметическое всех нечётных чисел от 1 до N. */
+
+console.log('=== Arithmetic mean of odd numbers: ===');
+
+const userInput = +prompt('Enter any number N: ')
+// const userInput = 3
+
+let sumArithMean = 0
+let count = 0
+let k = 1
+
+while (k <= userInput) {
+  sumArithMean += k
+  count++
+  k += 2
+}
+
+let arithmeticMean = sumArithMean / count
+
+console.log(arithmeticMean);
+console.log(`${arithmeticMean} is the arithmetic mean for ${userInput}`);
