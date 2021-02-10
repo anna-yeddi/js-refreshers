b82d207fec8dd2d37fd6f486e8403cfdddc3b2b3
Anna Kova <m3ta007@gmail.com>
2021-02-09 22:19:16 -0500

    Updated settings for ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
+  plugins: [
+    'jest'
+  ],
 };
