2090828e674078b1231c0499a51bbaa958b14528
Anna Kova <m3ta007@gmail.com>
2021-02-14 03:13:35 -0500

    Added tests for 03-loops -- sumOfIntegers50to100 and sevenTimes1to9 functions

--- src/03-loops.test.js
+++ src/03-loops.test.js
@@ -9,12 +9,43 @@ describe('03-loops.js', () => {
   describe('sumOfIntegers50to100', () => {
     it('is a function', () => {
       expect(typeof sumOfIntegers50to100).toBe('function')
+    }),
+    it('console.log a number', () => {
+      console.log = jest.fn();
+      sumOfIntegers50to100();
+      expect(typeof console.log.mock.calls[0][0]).toBe('number')
+    }),
+    it('console.log 3825', () => {
+      console.log = jest.fn();
+      sumOfIntegers50to100();
+      expect(console.log.mock.calls[0][0]).toBe(3825)
     })
+    
   });
   
   describe('sevenTimes1to9', () => {
     it('is a function', () => {
       expect(typeof sevenTimes1to9).toBe('function')
+    }),
+    it('console.log a string', () => {
+      console.log = jest.fn();
+      sevenTimes1to9();
+      expect(typeof console.log.mock.calls[0][0]).toBe('string')
+    }),
+    it('console.log first line "7 x 1 = 7"', () => {
+      console.log = jest.fn();
+      sevenTimes1to9();
+      expect(console.log.mock.calls[0][0]).toBe('7 x 1 = 7')
+    }),
+    it('console.log last/ninth line "7 x 9 = 63"', () => {
+      console.log = jest.fn();
+      sevenTimes1to9();
+      expect(console.log.mock.calls[8][0]).toBe('7 x 9 = 63')
+    }),
+    it('console.log 9 for a number of lines logged"', () => {
+      console.log = jest.fn();
+      sevenTimes1to9();
+      expect(console.log.mock.calls.length).toBe(9)
     })
   });
   
