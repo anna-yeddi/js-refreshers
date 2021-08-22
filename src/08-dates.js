b233107de1beed8f104892a34696a6aec4531411
Anna Kova <m3ta007@gmail.com>
2021-08-22 15:01:07 -0400

    updated tests for user input for 08-dates.js

--- src/08-dates.js
+++ src/08-dates.js
@@ -10,14 +10,14 @@
   * более молодого пользователя.
  */
 
-const dateInput = prompt('Enter a date in the format: DD.MM.YYYY');
-// const dateInput = '08.08.2021';
-
 /**
  * 1.Запросите у пользователя дату в формате ДД.ММ.ГГГГ. 
  * Напишите программу, выводящую день недели по введённой дате.
  */
-export function getDay(dateInput) {
+export function getDay() {
+  const dateInput = prompt('Enter a date in the format: DD.MM.YYYY');
+  // const dateInput = '08.08.2021';
+  
   const days = [
     'Monday',
     'Tuesday',
@@ -27,8 +27,8 @@ export function getDay(dateInput) {
     'Saturday',
     'Sunday'
   ]
-
-  const [date, month, year] = dateInput.toString().split('.');
+  
+  const [date, month, year] = dateInput.split('.');
   const dayFull = `${year}-${month}-${date}`;
   const dayNum = new Date(dayFull).getDate() - 1;
   const dayStr = (dayNum === -1) ? days[6] : days[dayNum];
@@ -50,4 +50,30 @@ export function minutesToday() {
   const minutesToday = (nowTs - todayDateTs) / 1000 / 60;
 
   console.log(minutesToday.toFixed(0));
+}
+
+/**
+ * 3.*В двух переменных хранятся даты рождения двух пользователей 
+  * в формате ДД.ММ.ГГГГ. Написать программу, которая определяет 
+  * более молодого пользователя.
+ */
+
+const bday1 = '20.02.2002';
+const bday2 = '11.11.1111';
+
+export function youngest(bday1, bday2) {
+  function getDate(str) {
+    const [date, month, year] = str.toString().split('.');
+    const day = `${year}-${month}-${date}`;
+
+    return day;
+  }
+
+  const bdayDate1 = Date.parse(getDate(bday1));
+  const bdayDate2 = Date.parse(getDate(bday2));
+  const youngestDate = new Date(Math.min(bdayDate1, bdayDate2));
+  
+  const youngestStr = (youngestDate === bdayDate1) ? bday1 : bday2;
+
+  return youngestStr;
 }
--- src/08-dates.test.js
+++ src/08-dates.test.js
@@ -1,12 +1,8 @@
-import { getDay, minutesToday } from "./08-dates.js";
+import { getDay, minutesToday, youngest } from "./08-dates.js";
 
 describe('getDay', () => {
   it('is a function', () => {
     expect(getDay).toBeInstanceOf(Function)
-  }),
-    
-  it('returns a string', () => {
-    expect(typeof getDay('08.08.2021')).toBe('string');
   }),
     
   describe('functional tests with user input', () => {
@@ -21,11 +17,22 @@ describe('getDay', () => {
       window.prompt = originalPrompt
     })
     
-    it('outputs a day of week', () => {
+    it('returns a string', () => {
+      jest.spyOn(window, 'prompt').mockReturnValue('08.08.2021');
       console.log = jest.fn();
-      getDay('08.08.2021');
-      
+
+      expect(typeof getDay('08.08.2021')).toBe('string');
       expect(typeof console.log.mock.calls[0][0]).toBe('string');
+    }),
+    
+    it('outputs a day of week', () => {
+      jest.spyOn(window, 'prompt').mockReturnValue('08.08.2021');
+      console.log = jest.fn();
+
+      getDay();
+      
+      expect(typeof getDay()).toBe('string');
+      expect(getDay()).toBe('Sunday');
       expect(console.log.mock.calls[0][0]).toBe('Sunday');
       
       const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -58,7 +65,28 @@ describe('minutesToday', () => {
       minutesToday();
 
       expect(typeof +console.log.mock.calls[0][0]).toBe('number');
-      expect(console.log.mock.calls[0][0] < (24 * 60)).toBeTruthy()
+      expect(+console.log.mock.calls[0][0]).toBeLessThan(24 * 60)
+    })
+  })
+})
+
+describe('youngest', () => {
+  it('is a function', () => {
+    expect(youngest).toBeInstanceOf(Function)
+  }),
+  
+  describe('functional tests', () => {
+    it('returns a string', () => {
+      expect(typeof youngest('20.02.2002', '11.11.1111')).toBe('string');
+    }),
+    
+    it('returns a birthday string', () => {
+      const arr = ['20.02.2002', '11.11.1111'];
+      const expected = new Array(youngest('20.02.2002', '11.11.1111'));
+      
+      expect(arr).toEqual(expect.arrayContaining(expected))
+      expect(youngest('20.02.2002', '11.11.1111')).toBe('11.11.1111');
+      expect(youngest('22.22.2222', '21.22.2222')).toBe('21.22.2222');
     })
   })
 })
