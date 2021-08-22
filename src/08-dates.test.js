8a32b67447d6497a586c307726220725b38e5a3f
Anna Kova <m3ta007@gmail.com>
2021-08-22 17:24:30 -0400

    updated tests for user input for 08-dates.js

--- src/08-dates.test.js
+++ src/08-dates.test.js
@@ -3,7 +3,7 @@ import { getDay, minutesToday, youngest } from "./08-dates.js";
 describe('getDay', () => {
   it('is a function', () => {
     expect(getDay).toBeInstanceOf(Function)
-  }),
+  });
     
   describe('functional tests with user input', () => {
     let originalConsoleLog;
@@ -11,19 +11,19 @@ describe('getDay', () => {
     beforeEach(() => {
       originalConsoleLog = console.log;
       originalPrompt = window.prompt;
-    })
+    });
     afterEach(() => {
       console.log = originalConsoleLog;
       window.prompt = originalPrompt
-    })
+    });
     
     it('returns a string', () => {
       jest.spyOn(window, 'prompt').mockReturnValue('08.08.2021');
       console.log = jest.fn();
 
-      expect(typeof getDay('08.08.2021')).toBe('string');
+      expect(typeof getDay()).toBe('string');
       expect(typeof console.log.mock.calls[0][0]).toBe('string');
-    }),
+    });
     
     it('outputs a day of week', () => {
       jest.spyOn(window, 'prompt').mockReturnValue('08.08.2021');
@@ -38,11 +38,11 @@ describe('getDay', () => {
       const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
       expect(days).toEqual(
         expect.arrayContaining([console.log.mock.calls[0][0]])
-      )
+      );
       expect(days).toEqual(
         expect.arrayContaining([getDay()])
       )
-    })
+    });
   })
 })
 
@@ -57,11 +57,11 @@ describe('minutesToday', () => {
     beforeEach(() => {
       originalConsoleLog = console.log;
       originalPrompt = window.prompt;
-    })
+    });
     afterEach(() => {
       console.log = originalConsoleLog;
       window.prompt = originalPrompt
-    })
+    });
       
     it('console.logs a number of minutes', () => {
       console.log = jest.fn();
@@ -76,20 +76,20 @@ describe('minutesToday', () => {
 describe('youngest', () => {
   it('is a function', () => {
     expect(youngest).toBeInstanceOf(Function)
-  }),
+  })
   
   describe('functional tests', () => {
     it('returns a string', () => {
       expect(typeof youngest('20.02.2002', '11.11.1111')).toBe('string');
-    }),
+    });
     
-    it('returns a birthday string', () => {
-      const arr = ['20.02.2002', '11.11.1111'];
-      const expected = new Array(youngest('20.02.2002', '11.11.1111'));
+      it('returns one of submitted birthday strings', () => {
+        const arr = ['20.02.2002', '11.11.1111'];
+        const expected = new Array(youngest('20.02.2002', '11.11.1111'));
       
-      expect(arr).toEqual(expect.arrayContaining(expected))
-      expect(youngest('20.02.2002', '11.11.1111')).toBe('11.11.1111');
-      expect(youngest('22.22.2222', '21.22.2222')).toBe('21.22.2222');
-    })
+        expect(arr).toEqual(expect.arrayContaining(expected))
+        expect(youngest('20.02.2002', '11.11.1111')).toBe('11.11.1111');
+        expect(youngest('22.22.2222', '21.22.2222')).toBe('21.22.2222');
+      });
   })
 })
